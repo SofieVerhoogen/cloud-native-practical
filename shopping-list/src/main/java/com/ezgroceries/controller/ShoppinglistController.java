a3092d9ad7ff1c355e57a07d724a8b2dea6da3c3
SofieVerhoogen <73700526+SofieVerhoogen@users.noreply.github.com>
2020-11-04 09:55:34 +0100

    test

---
+++ shopping-list/src/main/java/com/ezgroceries/controller/ShoppinglistController.java
@@ -0,0 +1,9 @@
+package com.ezgroceries.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/shopping-lists")
+public class ShoppinglistController {
+}
