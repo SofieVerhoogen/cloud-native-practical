0967b01d99a6d74b06ae53f9bd744aa642c7bc1c
SofieVerhoogen <73700526+SofieVerhoogen@users.noreply.github.com>
2020-11-04 09:55:34 +0100

    test

--- shopping-list/src/main/java/com/ezgroceries/service/CocktailResource.java
+++ shopping-list/src/main/java/com/ezgroceries/service/CocktailResource.java
@@ -1,9 +1,23 @@
 package com.ezgroceries.service;
 
+import com.ezgroceries.model.Cocktail;
+
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
 public class CocktailResource {
-    public CocktailResource(UUID fromString, String margerita, String cocktail_glass, String s, String s1, List<String> asList) {
+
+    private Cocktail cocktail;
+    public CocktailResource(){
+
+    }
+    public CocktailResource(UUID uuid, String cocktailName, String cocktail_glass, String instructions, String image, List<String> ingredients) {
+        cocktail.setCocktailID(uuid);
+        cocktail.setName(cocktailName);
+        cocktail.setGlass(cocktail_glass);
+        cocktail.setInstructions(instructions);
+        cocktail.setInstructions(image);
+        cocktail.setIngredients((ArrayList) ingredients);
     }
 }
