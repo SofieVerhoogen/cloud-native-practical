26c7b0e10c0e5971c3c659281e8b646632f40f12
SofieVerhoogen <73700526+SofieVerhoogen@users.noreply.github.com>
2020-11-17 20:20:01 +0100

    changes

--- shopping-list/src/main/java/com/ezgroceries/model/Cocktail.java
+++ shopping-list/src/main/java/com/ezgroceries/model/Cocktail.java
@@ -1,66 +1,6 @@
 package com.ezgroceries.model;
-import javax.swing.*;
-import java.awt.*;
-import java.util.ArrayList;
-import java.util.UUID;
+
 
 public class Cocktail {
 
-    private UUID cocktailID;
-    private String name;
-    private String glass;
-    private String instructions;
-    private Image image;
-    private List ingredients;
-
-    public long getCocktailID() {
-        return cocktailID;
     }
-
-    public void setCocktailID(UUID cockttailID) {
-        this.cocktailID = cockttailID;
-    }
-
-    public Image getImage() {
-        return image;
-    }
-
-    public void setImage(Image image) {
-        this.image = image;
-    }
-
-    public ArrayList getIngredients() {
-        return ingredients;
-    }
-
-    public void setIngredients(ArrayList ingredients) {
-        this.ingredients = ingredients;
-    }
-
-    public Cocktail(){
-
-    }
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getGlass() {
-        return glass;
-    }
-
-    public void setGlass(String glass) {
-        this.glass = glass;
-    }
-
-    public String getInstructions() {
-        return instructions;
-    }
-
-    public void setInstructions(String instructions) {
-        this.instructions = instructions;
-    }
-}
